4936d62f1663f915634c99ac00574b2f965a7874
venikpapa <venikpapa@gmail.com>
2022-04-11 01:26:53 +0300

    hide POLYGON_API_KEY from config

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,13 +1,15 @@
 require("@nomiclabs/hardhat-waffle");
 require("dotenv").config()
 
+const POLYGON_API_KEY=process.env.POLYGON_API_KEY || ""
+
 module.exports = {
   solidity: "0.8.4",
   defaultNetwork: "hardhat",
   networks: {
     hardhat: {
       forking: {
-          url: "https://polygon-mainnet.g.alchemy.com/v2/ExgmkRKbF0pAYQ6YLkMjfuDcjr1mUF5h",
+          url: POLYGON_API_KEY,
           blockNumber: 26969990,
       },
     },    
